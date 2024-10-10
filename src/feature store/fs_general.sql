71a94c262bdb35250067fcfca89b2caacb826072
RoseIFOS <roseif88@gmail.com>
2024-10-10 16:06:53 -0300

    Ajuste Feature Store

--- src/feature store/fs_general.sql
+++ src/feature store/fs_general.sql
@@ -1,12 +1,12 @@
 
 -- Base ativa é quem esta em uma janela de 21 dias
--- 2024-06-04 (última data da base, today)
+-- {date} (última data da base, today)
 
 with tb_rfv as (
 SELECT
     idCustomer,
     -- Calculando a recência
-    cast(min(julianday('2024-06-04') - julianday(dtTransaction)) 
+    cast(min(julianday('{date}') - julianday(dtTransaction)) 
     as INTERGER) + 1 AS recenciaDias,
 
     -- Calculando a frequencia
@@ -19,8 +19,8 @@ SELECT
     ) as valorPoints
 
 FROM transactions
-WHERE dtTransaction < '2024-06-04'
-AND dtTransaction >= DATE ('2024-06-04', '-21 day')
+WHERE dtTransaction < '{date}'
+AND dtTransaction >= DATE ('{date}', '-21 day')
 
 GROUP BY idCustomer),
     
@@ -29,7 +29,7 @@ tb_idade as (SELECT
 	
     -- Calculando a recência dos usuários da base ativa
 	
-    cast(max(julianday('2024-06-04') - julianday(t2.dtTransaction))
+    cast(max(julianday('{date}') - julianday(t2.dtTransaction))
     as INTERGER) + 1 AS idadeBaseDias
     
 FROM tb_rfv as t1
@@ -40,6 +40,7 @@ on t1.idcustomer = t2.idcustomer
 group by t2.idcustomer)
 
 SELECT 
+    '{date}' as dtRef,
     t1.*,
     t2.idadeBaseDias,
     t3.flEmail
