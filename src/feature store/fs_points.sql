d8b9ee5be5205b541a45d6685b9dcf4d13e812f7
RoseIFOS <roseif88@gmail.com>
2024-10-14 18:10:10 -0300

    Feature Store de Pontos

--- src/feature store/fs_points.sql
+++ src/feature store/fs_points.sql
@@ -1,9 +1,26 @@
+WITH tb_pontos_d as (
+
 SELECT 
 
+
     idCustomer,
-    sum(pointsTransaction) as saldoPoints,
+
+    -- CALCULANDO SALDO EM PONTOS
+
+    sum(pointsTransaction) as saldoPointsD21,
+
+    sum(CASE 
+            WHEN dtTransaction >= DATE('{date}', '-14 day') 
+            THEN pointsTransaction ELSE 0 
+            END) AS saldoPointsD14,
+
+    sum(CASE 
+            WHEN dtTransaction >= DATE('{date}', '-7 day') 
+            THEN pointsTransaction ELSE 0 
+            END) AS saldoPointsD7,
 
 
+   -- CALCULANDO PONTOS ACUMULADOS
     sum(CASE
             WHEN pointsTransaction > 0 THEN  pointsTransaction
             ELSE 0
@@ -11,18 +28,20 @@ SELECT
 
     sum(CASE
             WHEN pointsTransaction > 0 
-            AND dtTransaction >= date ('2024-06-04', '-14 day')
+            AND dtTransaction >= date ('{date}', '-14 day')
             THEN  pointsTransaction
             ELSE 0
         END ) as pointsAcumuladosD14,
 
     sum(CASE
             WHEN pointsTransaction > 0 
-            AND dtTransaction >= date ('2024-06-04', '-7 day')
+            AND dtTransaction >= date ('{date}', '-7 day')
             THEN  pointsTransaction
             ELSE 0
         END ) as pointsAcumuladosD7,
 
+
+   -- CALCULANDO PONTOS RESGATADOS
     sum(CASE
             WHEN pointsTransaction < 0 THEN  pointsTransaction
             ELSE 0
@@ -30,14 +49,14 @@ SELECT
 
     sum(CASE
             WHEN pointsTransaction < 0 
-            AND dtTransaction >= date ('2024-06-04', '-14 day')
+            AND dtTransaction >= date ('{date}', '-14 day')
             THEN  pointsTransaction
             ELSE 0
         END ) as pointsResgatadosD14,
 
     sum(CASE
             WHEN pointsTransaction < 0 
-            AND dtTransaction >= date ('2024-06-04', '-7 day')
+            AND dtTransaction >= date ('{date}', '-7 day')
             THEN  pointsTransaction
             ELSE 0
         END ) as pointsResgatadosD7
@@ -45,7 +64,56 @@ SELECT
 
 FROM transactions
 
-WHERE dtTransaction < '2024-06-04'
-AND dtTransaction >=  DATE ('2024-06-04',  '-21 DAY')
+WHERE dtTransaction < '{date}'
+AND dtTransaction >=  DATE ('{date}',  '-21 DAY')
 
-GROUP BY idCustomer
+GROUP BY idCustomer),
+
+tb_vida as (
+
+-- CALCULANDO O SALDO CONSIDERANDO TODA A JORNADA (saldo de pontos na vida)
+SELECT 
+
+    t1.idCustomer,
+    sum(t2.pointsTransaction) as saldoPoints,
+    sum(CASE
+            WHEN t2.pointsTransaction > 0 THEN  t2.pointsTransaction
+            ELSE 0
+        END ) as pointsAcumuladosVida,
+    sum(CASE
+            WHEN t2.pointsTransaction < 0 THEN  t2.pointsTransaction
+            ELSE 0
+        END ) as pointsResgatadosVida,
+
+    CAST(max(julianday('{date}') - julianday(dtTransaction)) as INTEGER) + 1 AS diasVida    
+
+FROM tb_pontos_d as T1
+
+LEFT JOIN transactions as T2
+ON t1.idCustomer = t2.idCustomer
+
+WHERE t2.dtTransaction < '{date}' -- Garante dado d-1
+
+GROUP BY t1.idCustomer),
+
+tb_join as (
+
+-- JUNTANDO AS 2 QUERIES
+
+
+SELECT 
+
+    t1.*,
+    t2.saldoPoints,
+    t2.pointsAcumuladosVida,
+    t2.pointsResgatadosVida,
+    1.0 * t2.pointsAcumuladosVida / t2.diasVida as pointsPorDia
+
+FROM 
+    tb_pontos_d as t1
+    
+LEFT JOIN tb_vida as t2
+ON t1.idCustomer = t2.idCustomer)
+
+SELECT * FROM tb_join
+
